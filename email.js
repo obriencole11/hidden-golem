83b2d1a40a9e10a1d385f0156b64a4781a56400c
Cole O'Brien <>
2019-09-22 22:58:57 -0700

    Initial commit

---
+++ email.js
@@ -0,0 +1,16 @@
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwRcf5IdcSWQL4O4iudYU71zQck-UEGcSdhm20QioR4ZKJ2edNm/exec'
+
+$('#submit-form').on('click', function(e) {
+    console.log('test');
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+    throw new Error('test');
+  );
+})
